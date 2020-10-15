609f2e4051afe1c3f819a8e9dc861759ef57ce62
Warkittin22 <63461879+Warkittin22@users.noreply.github.com>
2020-10-15 11:25:41 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,8 @@
+// Change background to red
+function setRed() {
+ document.body.style.background = "red";
+}
+// Change background to white
+function setWhite() {
+ document.body.style.background = "white";
+}
